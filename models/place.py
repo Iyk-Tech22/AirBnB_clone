713b8785c1aae96d9552695e42eb4de99c7ae9d6
AngelUche <ucheorji9974@gmail.com>
2022-11-24 04:51:16 +0100

    updated mandatory

---
+++ models/place.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+from models.base_model import BaseModel
+class Place (BaseModel):
+    """
+    inherits from BaseModel
+    """
+    city_id = " "
+    user_id = " "
+    name =" "
+    description = " "
+    number_roomsInt = 0
+    number_bathrooms:int = 0
+    max_quest:int = 0
+    price_by_night:int = 0
+    latitude:float = 0.0
+    longitude:float = 0.0
+    amenity = []
