b86068980c8482c3e16abe357bbeef094e0175a4
Iyk Faiz <ikechukwugodwin22@yahoo.com>
2022-11-26 16:22:29 +0100

    Update console file added advc task feature

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 
 
 import cmd
+import re
+from shlex import shlex
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
@@ -25,6 +27,33 @@ class HBNBCommand(cmd.Cmd):
         "print a newline after exiting the console"
         print()
 
+    def onecmd(self, arg):
+        """ onecmd is a func that is called by cmd.Cmd
+            before the args  dispach to the right cmd
+        """
+        try:
+            if "." in arg and ")" in arg:
+                args = arg.split(".")
+                cls_name = args[0]
+                cls_id = ""
+                attr_name = ""
+                attr_val = ""
+                res = args[1].strip(")").split("(")
+                if len(res) == 1:
+                    cmd_name = res[0]
+                elif "," not in res[1] and len(res) == 2:
+                    cmd_name = res[0]
+                    cls_id = res[1].strip('"')
+                else:
+                    cmd_name = res[0]
+                    res = res[1].split(",")
+                    cls_id = res[0].replace('"', '')
+                    attr_name = res[1]
+                    attr_val = res[2]
+                arg = f"{cmd_name} {cls_name} {cls_id} {attr_name} {attr_val}"
+        finally:
+            return cmd.Cmd.onecmd(self, arg)
+
     def parseline(self, arg):
         "return tuple of the cmd and the args"
         res = cmd.Cmd.parseline(self, arg)
@@ -45,38 +74,27 @@ class HBNBCommand(cmd.Cmd):
         """show command: show instance base on the cls_name and cls_id"""
         def action(cls_name, cls_id, attr_name, attr_val):
             "get an instance from the file storage"
-            self.storage.reload()
             objs = self.storage.all()
-            found = False
-            for key, value in objs.items():
-                key = key.split(".")
-                if cls_name in key and cls_id in key:
-                    print(value.__str__())
-                    found = True
-                    break
-            if not found:
+            key = f"{cls_name}.{cls_id}"
+            if key in objs.keys():
+                print(objs[key])
+            else:
                 print("** no instance found **")
-                self.validate_cmd_arg(arg, action)
+        self.validate_arg(arg, action)
 
     def do_destroy(self, arg):
         """destroy command: destroy an instance base on the class and id"""
 
         def action(cls_name, cls_id, attr_name, attr_val):
             "delete a instance and save to file"
-            found = False
-            self.storage.reload()
             objs = self.storage.all()
-            for key in objs.keys():
-                key_c = key.split(".")
-                if cls_name in key_c and cls_id in key_c:
-
-                    del objs[key]
-                    self.storage.save()
-                    found = True
-                    break
-            if not found:
+            key = f"{cls_name}.{cls_id}"
+            if key in objs.keys():
+                del objs[key]
+                self.storage.save()
+            else:
                 print("** no instance found **")
-        self.validate_cmd_arg(arg, action)
+        self.validate_arg(arg, action)
 
     def do_all(self, arg):
         """all opt: Print all str repr of an
@@ -85,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
         objs = self.storage.all()
         if not arg:
             print([value.__str__() for key,
-                   value in objs.items() if "BaseModel" in key.split(".")])
+                   value in objs.items()])
         elif arg in self.Classes_k:
             print([value.__str__() for key,
                    value in objs.items() if arg in key.split(".")])
@@ -99,25 +117,37 @@ class HBNBCommand(cmd.Cmd):
         def action(cls_name, cls_id, attr_name, attr_val):
             "update attr or add new attr at time"
             objs = self.storage.all()
-            found = False
-            for key, value in objs.items():
-                key_c = key.split(".")
-                if cls_name in key_c and cls_id in key_c:
-                    if not attr_name:
-                        print("** attribute name missing **")
-                    elif not attr_val:
-                        print("** value missing **")
-                    else:
-                        obj_to_update = value.to_dict()
-                        del objs[key]
-                        obj_to_update[attr_name] = attr_val
-                        obj_to_update = self.Classes[cls_name](**obj_to_update)
-                        obj_to_update.save()
-                    found = True
-                    break
-            if not found:
+            key = f"{cls_name}.{cls_id}"
+            if key in objs.keys():
+                if not attr_name:
+                    print("** attribute name missing **")
+                elif not attr_val:
+                    print("** value missing **")
+                else:
+                    obj_to_update = objs[key].to_dict()
+                    del objs[key]
+                    obj_to_update[attr_name] = attr_val
+                    obj_to_update = self.Classes[cls_name](**obj_to_update)
+                    obj_to_update.save()
+            else:
                 print("** no instance found **")
-        self.validate_cmd_arg(arg, action)
+        self.validate_arg(arg, action)
+
+    def do_count(self, arg):
+        """count command: count the numbers of instances"""
+        objs = self.storage.all()
+        count = 0
+        if not arg:
+            for i in objs:
+                count += 1
+            print(count)
+        elif arg in self.Classes_k:
+            for key in objs.keys():
+                if arg in key.split("."):
+                    count += 1
+            print(count)
+        else:
+            print("** class doesn't exist **")
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -131,7 +161,7 @@ class HBNBCommand(cmd.Cmd):
         """Does nothing when the enterkey is pressed"""
         pass
 
-    def validate_cmd_arg(self, arg, action):
+    def validate_arg(self, arg, action):
         """validate the args of show, destroy....commands"""
         cls_name, cls_id, args = self.parseline(arg)
         if not arg:
@@ -149,7 +179,7 @@ class HBNBCommand(cmd.Cmd):
                     attr_name = args[2]
                 else:
                     attr_name = None
-                if len(args) == 4:
+                if len(args) >= 4:
                     attr_val = args[3]
                 else:
                     attr_val = None
